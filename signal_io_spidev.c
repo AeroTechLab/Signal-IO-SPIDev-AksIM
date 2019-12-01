cf82322e91420c97419eb96c19cf7271639754ad
Bitiquinho <consoni_2519@hotmail.com>
2019-12-01 10:59:04 -0300

    Make readings on a separate thread

--- signal_io_spidev.c
+++ signal_io_spidev.c
@@ -28,14 +28,22 @@
 #include <sys/ioctl.h>
 #include <asm/ioctl.h>
 #include <linux/spi/spidev.h>
+#include <pthread.h>
+
+#define CHANNELS_NUMBER 2
 
 const char* SPI_DEVS[] = { "/dev/spidev0.0", "/dev/spidev0.1" };
 
-int32_t inputValues[ 2 ] = { 0, 0 };
-int spiFDs[ 2 ] = { 0, 0 };
-struct spi_ioc_transfer spiIOCs[ 2 ];
+int32_t inputValues[ CHANNELS_NUMBER ] = { 0, 0 };
+int spiFDs[ CHANNELS_NUMBER ] = { 0, 0 };
+struct spi_ioc_transfer spiIOCs[ CHANNELS_NUMBER ];
+
+pthread_t threadID = 0;
+volatile bool isRunning = false;
 
 DECLARE_MODULE_INTERFACE( SIGNAL_IO_INTERFACE );
+
+static void* AsyncTransfer( void* args );
 
 long int InitDevice( const char* configuration )
 {  
@@ -43,7 +51,7 @@ long int InitDevice( const char* configuration )
   
   uint8_t mode = SPI_MODE_1;
   uint8_t bitsPerWord = 8;
-  for( size_t channel = 0; channel < 2; channel++ )
+  for( size_t channel = 0; channel < CHANNELS_NUMBER; channel++ )
   {
     if( spiFDs[ channel ] != 0 )
     {
@@ -63,6 +71,8 @@ long int InitDevice( const char* configuration )
     }
   }
   
+  if( !isRunning ) pthread_create( &threadID, NULL, AsyncTransfer, NULL );
+  
   if( spiFDs[ 0 ] < 0 || spiFDs[ 1 ] < 0 )
   {
     EndDevice( 0 );
@@ -76,6 +86,12 @@ void EndDevice( long int deviceID )
 {
   if( spiFDs[ 0 ] > 0 ) close( spiFDs[ 0 ] );
   if( spiFDs[ 1 ] > 0 ) close( spiFDs[ 1 ] );
+  
+  if( isRunning ) 
+  {
+    isRunning = false;
+    pthread_join( threadID, NULL );
+  }
   
   return;
 }
@@ -93,21 +109,7 @@ size_t Read( long int deviceID, unsigned int channel, double* ref_value )
   
   if( deviceID == SIGNAL_IO_DEVICE_INVALID_ID ) return 0;
   
-  if( channel > 2 ) return 0;
-   
-  // SPI transfer buffer is output (before) and input (after)
-  uint8_t inputData[ 2 ];
-  spiIOCs[ channel ].tx_buf = spiIOCs[ channel ].rx_buf = (uint64_t) inputData;
-  if( ioctl( spiFDs[ channel ], SPI_IOC_MESSAGE( 1 ), &(spiIOCs[ channel ]) ) < 0 )
-    fprintf( stderr, "Unable to read from SPI device %s: %s\n", SPI_DEVS[ channel ], strerror( errno ) );
-  uint16_t rawInputValue = ( ( inputData[ 0 ] << 8 ) & 0xFF00 ) + inputData[ 1 ];
-  
-  int32_t overflowsNumber = inputValues[ channel ] / UINT16_MAX;
-  if( inputValues[ channel ] < 0 ) overflowsNumber--;
-  int32_t newInputValue = overflowsNumber * UINT16_MAX + rawInputValue;
-  if( ( inputValues[ channel ] - newInputValue ) > ( UINT16_MAX / 2 ) ) overflowsNumber++;
-  if( ( inputValues[ channel ] - newInputValue ) < ( -UINT16_MAX / 2 ) ) overflowsNumber--;
-  inputValues[ channel ] = overflowsNumber * UINT16_MAX + rawInputValue;
+  if( channel > CHANNELS_NUMBER ) return 0;
   
   *ref_value = (double) inputValues[ channel ];
 
@@ -128,7 +130,7 @@ bool CheckInputChannel( long int deviceID, unsigned int channel )
 {
   if( deviceID == SIGNAL_IO_DEVICE_INVALID_ID ) return false;
   
-  if( channel > 2 ) return false;
+  if( channel > CHANNELS_NUMBER ) return false;
   
   return true;
 }
@@ -148,3 +150,29 @@ void ReleaseOutputChannel( long int deviceID, unsigned int channel )
   return;
 }
 
+static void* AsyncTransfer( void* args )
+{  
+  isRunning = true;
+  
+  while( isRunning )
+  {
+    for( size_t channel = 0; channel < CHANNELS_NUMBER; channel++ )
+    {
+      // SPI transfer buffer is output (before) and input (after)
+      uint8_t inputData[ 2 ];
+      spiIOCs[ channel ].tx_buf = spiIOCs[ channel ].rx_buf = (uint64_t) inputData;
+      if( ioctl( spiFDs[ channel ], SPI_IOC_MESSAGE( 1 ), &(spiIOCs[ channel ]) ) < 0 )
+        fprintf( stderr, "Unable to read from SPI device %s: %s\n", SPI_DEVS[ channel ], strerror( errno ) );
+      uint16_t rawInputValue = ( ( inputData[ 0 ] << 8 ) & 0xFF00 ) + inputData[ 1 ];
+      
+      int32_t overflowsNumber = inputValues[ channel ] / UINT16_MAX;
+      if( inputValues[ channel ] < 0 ) overflowsNumber--;
+      int32_t newInputValue = overflowsNumber * UINT16_MAX + rawInputValue;
+      if( ( inputValues[ channel ] - newInputValue ) > ( UINT16_MAX / 2 ) ) overflowsNumber++;
+      if( ( inputValues[ channel ] - newInputValue ) < ( -UINT16_MAX / 2 ) ) overflowsNumber--;
+      inputValues[ channel ] = overflowsNumber * UINT16_MAX + rawInputValue;
+    }
+  }
+  
+  return NULL;
+}
